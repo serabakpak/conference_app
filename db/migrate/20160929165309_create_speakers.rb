4745e2f9889ad577d05dcde34a637c2bc0cc0380
Sera Bak <serabak15@gmail.com>
2016-09-29 09:55:30 -0700

    creates speaker model

---
+++ db/migrate/20160929165309_create_speakers.rb
@@ -0,0 +1,11 @@
+class CreateSpeakers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :speakers do |t|
+      t.string :first
+      t.string :last
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
